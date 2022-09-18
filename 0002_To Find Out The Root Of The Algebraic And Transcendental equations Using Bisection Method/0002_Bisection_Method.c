29f799a587d99d0acfcda4bcc8e0f6f89d98da73
VICKY <vishujoshi21nov2000@gmail.com>
2022-09-18 19:37:00 +0530

    Bisection Method

--- 0002_To Find Out The Root Of The Algebraic And Transcendental equations Using Bisection Method/0002_Bisection_Method.c
+++ 0002_To Find Out The Root Of The Algebraic And Transcendental equations Using Bisection Method/0002_Bisection_Method.c
@@ -52,3 +52,50 @@ int main()
     } while (fabs(fun(root)) > all);
     return 0;
 }
+
+/*
+// Write a program in “C” Language to find out the root of the Algebraic and Transcendental equations using Bisection Method. 
+#include<stdio.h>
+#include<math.h>
+#define f(x) (x*x*x)-(5*x)+1
+int main()
+{
+    float x0,x1,x2,f0,f1,f2,e;
+    int i=0;
+    printf("Enter the value of x0 : ");
+    scanf("%f",&x0);
+    printf("Enter the value of x1 : ");
+    scanf("%f",&x1);
+    printf("Enter the allowed error : ");
+    scanf("%f",&e);
+    if(f(x0)*f(x1)<0)
+    {
+        printf("Roots found\n");
+    }
+    else
+    {
+        printf("Wrong interval\n");
+        return 0;
+    }
+    do
+    {
+        f0=f(x0);
+        f1=f(x1);
+        x2=(x0+x1)/2;
+        f2=f(x2);
+        if(f0*f2<0)
+        {
+            x1=x2;
+        }
+        else
+        {
+            x0=x2;
+        }
+        i++;
+        printf("No of iteration = %d ",i);
+        printf("and Root = %f\n",x2);
+    }while(fabs(f2)>e);
+    printf("\nFinal root after %d iteration = %f\n",i,x2);
+    return 0;
+}
+*/
