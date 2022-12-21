cc2a1565b4b2ce80bddca4295ed2212d08df3c8c
VICKY <vishujoshi21nov2000@gmail.com>
2022-12-21 12:37:45 +0530

    Simpson's 1/3 Rule

---
+++ 0014 Simpson 1 by 3 Rule/simpson_1by3.c
@@ -0,0 +1,34 @@
+// Write a program in “C” Language to integrate numerically using Simpson’s 1/3 Rule.
+
+#include <stdio.h>
+
+float value(float x)
+{
+    return 1 / (1 + x * x);
+}
+
+int main()
+{
+    int n, position_of_term = 1;
+    float a, b, sum = 0, h;
+
+    printf("Enter the value of a and b : ");
+    scanf("%f%f", &a, &b);
+
+    printf("Enter the number of intervals : ");
+    scanf("%d", &n);
+
+    h = (b - a) / n;
+    sum = value(a) + value(b);
+
+    for (int i = a + h; i < b; i += h)
+    {
+        sum += (position_of_term % 2 == 0) ? 2 * value(i) : 4 * value(i);
+        position_of_term++;
+    }
+    sum = (h * sum) / 3;
+
+    printf("\nValue of the integral = %f\n", sum);
+
+    return 0;
+}
